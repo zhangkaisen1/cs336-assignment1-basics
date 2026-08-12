5049042fd8dbf18cc86854adcaf0b2514ade7847
zhangkaisen1 <1059700881@qq.com>
2026-08-13 00:40:34 +0800

    Implement Linear/embedding/RMSNorm/SwiGLU/Silu/RoPE/softmax/scaled_dot_attention

--- tests/adapters.py
+++ tests/adapters.py
@@ -28,8 +28,10 @@ def run_linear(
     Returns:
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
-
-    raise NotImplementedError
+    from cs336_basics.model import Linear
+    linear = Linear(d_in, d_out)
+    linear.load_state_dict({"w": weights})
+    return linear(in_features)
 
 
 def run_embedding(
@@ -50,9 +52,10 @@ def run_embedding(
     Returns:
         Float[Tensor, "... d_model"]: Batch of embeddings returned by your Embedding layer.
     """
-
-    raise NotImplementedError
-
+    from cs336_basics.model import Embedding
+    embedding = Embedding(vocab_size, d_model)
+    embedding.load_state_dict({"w": weights})
+    return embedding(token_ids)
 
 def run_swiglu(
     d_model: int,
@@ -83,7 +86,14 @@ def run_swiglu(
     # swiglu.w1.weight.data = w1_weight
     # swiglu.w2.weight.data = w2_weight
     # swiglu.w3.weight.data = w3_weight
-    raise NotImplementedError
+    from cs336_basics.model import SwiGLU
+    swiglu = SwiGLU(d_model, d_ff)
+    swiglu.load_state_dict({
+        "w1.w": w1_weight,
+        "w2.w": w2_weight,
+        "w3.w": w3_weight,
+    })
+    return swiglu(in_features)
 
 
 def run_scaled_dot_product_attention(
@@ -104,7 +114,8 @@ def run_scaled_dot_product_attention(
     Returns:
         Float[Tensor, " ... queries d_v"]: Output of SDPA
     """
-    raise NotImplementedError
+    from cs336_basics.model import scaled_dot_product_attention
+    return scaled_dot_product_attention(Q, K, V, mask)
 
 
 def run_multihead_self_attention(
@@ -200,7 +211,10 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+   # print(in_query_or_key.shape)
+    from cs336_basics.model import RotaryPositionalEmbedding
+    r =  RotaryPositionalEmbedding(theta, d_k, max_seq_len)
+    return r(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
@@ -378,7 +392,10 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    from cs336_basics.model import RMSNorm
+    rms_norm = RMSNorm(d_model, eps)
+    rms_norm.load_state_dict({"g": weights})
+    return rms_norm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
@@ -392,7 +409,8 @@ def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
         Float[Tensor,"..."]: of with the same shape as `in_features` with the output of applying
         SiLU to each element.
     """
-    raise NotImplementedError
+    from cs336_basics.model import SwiGLU
+    return SwiGLU.silu(in_features)
 
 
 def run_get_batch(
@@ -431,7 +449,8 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    from cs336_basics.model import softmax
+    return softmax(in_features, dim)
 
 
 def run_cross_entropy(
